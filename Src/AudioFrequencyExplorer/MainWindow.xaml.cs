015c6f385ddc735c2e5db540d6bed16f4ddfaa87
AlexPi <alex.pigida@outlook.com>
2020-12-24 23:19:12 -0500

    poc for testing acoustic systems

---
+++ Src/AudioFrequencyExplorer/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using AAV.Sys.Helpers;
+using AAV.WPF.AltBpr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AudioFrequencyExplorer
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      //Loaded += async (s, e) => await play();
+    }
+
+    void onFreq(object s, RoutedPropertyChangedEventArgs<double> e)
+    {
+      tbxFreq.Text = $" {freq(e.NewValue):N0} Hz ";
+      tbxWLen.Text = $" {wavelength(e.NewValue).ToString().Substring(0, 4)} m ";
+    }
+
+    void onDurn(object s, RoutedPropertyChangedEventArgs<double> e) => tbxDurn.Text = $" {e.NewValue:N0} ms ";
+
+    async void onPlay(object sender, RoutedEventArgs e) => await play();
+
+
+    void onClose(object sender, RoutedEventArgs e) => Close();
+    void chkIsAuto_Checked(object s, RoutedEventArgs e) { }
+
+    static double freq(double e) => Math.Pow(2, e);
+    static double wavelength(double e) => 344.007 / Math.Pow(2, e);
+    async Task play()
+    {
+      try
+      {
+        do
+        {
+          await ChimerAlt.BeepFD((int)freq(slrFreq.Value), slrDurn.Value * .001); //todo: .ConfigureAwait(false);
+          //if (chkIsKern.IsChecked == true)          {          }          else          {          }
+        } while (chkIsAuto.IsChecked == true);
+      }
+      catch (Exception ex) { tbxEx.Text = ex.Message; }
+    }
+
+
+  }
+}
